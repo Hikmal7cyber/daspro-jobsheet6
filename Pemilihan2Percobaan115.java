7a5e6869e1cdfa4e189ddc58eb93bee81e5a53df
Hikmal7cyber <mhikmal.ag@gmail.com>
2024-10-02 12:01:16 +0700

    Percobaan 1

---
+++ Pemilihan2Percobaan115.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan115 {
+    public static void main(String[] args){
+        Scanner input15 = new Scanner(System.in);
+        
+        System.out.print("Masukkan Tahun: ");
+        int tahun15 = input15.nextInt();
+
+        if ((tahun15 % 4) == 0) {
+            if ((tahun15 % 100) == 0)
+                System.out.println("Tahun Kabisat");
+        }   else 
+                System.out.println("Bukan Tahun Kabisat");
+    }
+}
