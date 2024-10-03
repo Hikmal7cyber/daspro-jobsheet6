20e69dd680be879cc3eab59e370d7e34f59353f7
Hikmal7cyber <mhikmal.ag@gmail.com>
2024-10-03 23:27:37 +0700

    Implementasi Flowchart Latihan Mandiri 2

--- LatihanMandiri215.java
+++ LatihanMandiri215.java
@@ -4,24 +4,40 @@ public class LatihanMandiri215 {
     public static void main(String[] args) {
         Scanner input15 = new Scanner(System.in);
 
-        double hargaBuku = 20000;
-        double diskon = 0;
-        double totalHarga;
+        double diskon = 0.0;
+        int jumlahBuku, jumlahDiskon, total, hargaBuku = 20000;
+        String jenisBuku;
 
-        System.out.println("Masukkan jenis buku (kamus/novel/lainnya): ");
-        String jenisBuku = input15.nextLine();
-        System.out.println("Masukkan jumlah buku: ");
-        int jumlahBuku = input15.nextInt();
+        System.out.print("Masukan Jenis buku: ");
+        jenisBuku = input15.nextLine();
+        System.out.print("Masukan Jumlah buku: ");
+        jumlahBuku = input15.nextInt();
 
         if (jenisBuku.equalsIgnoreCase("kamus")) {
             diskon = 0.1;
+            if (jumlahBuku > 2) {
+                diskon += 0.02 ;
+            }
         } else if (jenisBuku.equalsIgnoreCase("novel")) {
             diskon = 0.07;
-        } 
+            if  (jumlahBuku > 3) {
+                diskon += 0.02;
+            } else {
+                 if (jumlahBuku <= 3)
+                diskon += 0.01;
+            }
+        } else {
+            if (jumlahBuku>3) {
+                diskon = 0.05;
 
-        totalHarga = (hargaBuku * jumlahBuku) - (hargaBuku * jumlahBuku * diskon);
+            }
+        }
+        double hargaTotal = hargaBuku * jumlahBuku;
+        double totalDiskon = hargaTotal* diskon;
+        total = (int) (hargaBuku * jumlahBuku - totalDiskon);
 
-
+        System.out.println("Total Diskon: " + totalDiskon);
+        System.out.println("Total Bayar: " + total );
     }
 }
 
